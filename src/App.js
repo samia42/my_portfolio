54826cf4cf8a773126eefd36a8c1b01e2f8fb7df
samia <samiasaif42@gmail.com>
2023-06-05 19:27:36 +0500

    ..

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
   return (
    <>
    <Routes>
-        <Route exact path='/my_portfolio' element={<Layout/>}>
+        <Route exact path='/my_portfolio/index.html' element={<Layout/>}>
           {/* <Route index element={<Home />} /> */}
 
       </Route>
