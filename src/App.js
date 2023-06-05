e8bfcec4b4ff6955e1b68292cba89516de7e7de9
samia <samiasaif42@gmail.com>
2023-06-05 18:49:55 +0500

    ...

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
   return (
    <>
    <Routes>
-      <Route path='/' element={<Layout/>}>
+        <Route path='/my_portfolio' element={<Layout/>}>
           <Route index element={<Home />} />
 
       </Route>
